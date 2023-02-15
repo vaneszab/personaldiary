a5c0eede739714f80a16e51e1ccd53762de55678
unknown <vnssaab123@gmail.com>
2023-02-15 10:12:23 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+connect_string='mongodb+srv://vanessa:vanessa@learningxint.ccdzb9r.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(connect_string)
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary_lsv.find({}, {'_id': False}))
+    return jsonify({'articles': articles})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    # sample_receive = request.form.get('sample_give')
+    # print(sample_receive)
+    file = request.files['file_give']
+    extension = file.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'file-{mytime}.{extension}'
+    save_to = f'static/{filename}'
+    file.save(save_to)
+
+    profile = request.files['profile_give']
+    extension = profile.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    profile_name = f'profile-{mytime}.{extension}'
+    save_to = f'static/{profile_name}'
+    profile.save(save_to)
+
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
+    doc = {
+        'file': filename,
+        'profile': profile_name,
+        'title': title_receive,
+        'content': content_receive
+    }
+    db.diary_lsv.insert_one(doc)
+    return jsonify({'message': 'data was saved!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
